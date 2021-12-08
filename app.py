a3d058a8541f5518ae83fde5d2ad8d1c37418341
hp <gautammps123456789@gmail.com>
2021-12-08 12:51:04 +0530

    yo

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+state = 0
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('lnoN3pNo/DUuie5L3OT9exNM+/WZzquIkqGIZdVFcOTHOAhdkNe8IXDilhrKQNrFLQViNJv0MVcZtIzaU7sFKoOkc9s657sq5xb64EtiVqbCoDPEwqt0xwZgkuFriqVOVKVQrP7sXhjR4dNQm5Gk1QdB04t89/1O/w1cDnyilFU=')
@@ -38,10 +40,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     """ Here's all the messages will be handled and processed by the program """
+
+    response = ''
+
+    if state == 0:
+        response = "選択肢一つを選択してください。\n 1.運動 \n 2. 食事  \n 3. 姿勢 \n 4. 記録"
+
     print(event)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=response))
 
 
 if __name__ == "__main__":
