f4279ae85dc331743f12800c3218f989c7a94871
hp <gautammps123456789@gmail.com>
2022-03-31 03:13:04 +0530

    golb8

--- app.py
+++ app.py
@@ -119,6 +119,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     """ Here's all the messages will be handled and processed by the program """
+    print("handleevent")
+    print(event)
     if event.type == "message": 
         # #print(event)
         resp = statehandle(event)
