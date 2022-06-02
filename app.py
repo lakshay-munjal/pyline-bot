6ceacfb17b979a10e050c53d9958c602bd94b671
hp <shreshth29sapra@gmail.com>
2022-06-03 04:54:16 +0530

    ff

--- app.py
+++ app.py
@@ -269,22 +269,82 @@ def statehandle(event):
         response = "rebooted"
 
         dict1 = {
-            "type": "bubble", 
-            "body": { 
-                "type": "box", 
-                "layout": "horizontal", 
-                "contents": [ 
+            "type": "bubble",
+            "size": "kilo",
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
                 {
-                    "type": "text", 
-                    "text": "Hello,"
+                    "type": "text",
+                    "text": "姿勢画像の送信",
+                    "weight": "bold",
+                    "size": "lg",
+                    "align": "center"
                 },
                 {
-                    "type": "text", 
-                    "text": "World!"
+                    "type": "text",
+                    "text": "どちらで送信しますか？",
+                    "wrap": True,
+                    "align": "center",
+                    "margin": "md"
+                },
+                {
+                    "type": "separator",
+                    "margin": "sm"
+                },
+                {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "margin": "md",
+                    "spacing": "sm",
+                    "contents": [
+                    {
+                        "type": "button",
+                        "action": {
+                        "type": "uri",
+                        "label": "カメラ",
+                        "uri": "https://line.me/R/nv/camera/",
+                        "altUri": {
+                            "desktop": "https://line.me/R/nv/camera/"
+                        }
+                        },
+                        "style": "secondary",
+                        "height": "sm"
+                    },
+                    {
+                        "type": "button",
+                        "action": {
+                        "type": "uri",
+                        "label": "画像",
+                        "uri": "https://line.me/R/nv/cameraRoll/single",
+                        "altUri": {
+                            "desktop": "https://line.me/R/nv/cameraRoll/single"
+                        }
+                        },
+                        "style": "secondary",
+                        "height": "sm",
+                        "adjustMode": "shrink-to-fit"
+                    }
+                    ]
                 }
                 ]
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": []
+            },
+            "styles": {
+                "body": {
+                "backgroundColor": "#F4F3F9"
+                },
+                "footer": {
+                "separator": True,
+                "backgroundColor": "#F4F3F9"
+                }
             }
-            }
+        }
 
         line_bot_api.push_message(
             event.source.user_id,
