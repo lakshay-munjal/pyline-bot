965df49200481c53ad4ba0b92a70bef30d28c20d
hp <shreshth29sapra@gmail.com>
2022-06-29 00:12:39 +0530

    ff

--- util.py
+++ util.py
@@ -3,81 +3,75 @@
 
 def func():
     dict1 = {
-            "type": "bubble",
-            "size": "kilo",
-            "body": {
+        "type": "bubble",
+        "size": "kilo",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "姿勢(座位)",
+                "weight": "bold",
+                "size": "lg",
+                "align": "center"
+            },
+            {
+                "type": "text",
+                "text": "どの方向から診断しますか？",
+                "wrap": True,
+                "align": "center",
+                "margin": "md"
+            },
+            {
+                "type": "separator",
+                "margin": "sm"
+            },
+            {
                 "type": "box",
-                "layout": "vertical",
+                "layout": "horizontal",
+                "margin": "md",
+                "spacing": "sm",
                 "contents": [
                 {
-                    "type": "text",
-                    "text": "姿勢画像の送信 lawddde~",
-                    "weight": "bold",
-                    "size": "lg",
-                    "align": "center"
-                },
-                {
-                    "type": "text",
-                    "text": "どちらで送信しますか？",
-                    "wrap": True,
-                    "align": "center",
-                    "margin": "md"
-                },
-                {
-                    "type": "separator",
-                    "margin": "sm"
-                },
-                {
-                    "type": "box",
-                    "layout": "horizontal",
-                    "margin": "md",
-                    "spacing": "sm",
-                    "contents": [
-                    {
-                        "type": "button",
-                        "action": {
-                        "type": "uri",
-                        "label": "カメラ",
-                        "uri": "https://line.me/R/nv/camera/",
-                        "altUri": {
-                            "desktop": "https://line.me/R/nv/camera/"
-                        }
-                        },
-                        "style": "secondary",
-                        "height": "sm"
+                    "type": "button",
+                    "action": {
+                    "type": "message",
+                    "label": "左側",
+                    "text": "座位で左側から診断"
                     },
-                    {
-                        "type": "button",
-                        "action": {
-                        "type": "uri",
-                        "label": "画像",
-                        "uri": "https://line.me/R/nv/cameraRoll/single",
-                        "altUri": {
-                            "desktop": "https://line.me/R/nv/cameraRoll/single"
-                        }
-                        },
-                        "style": "secondary",
-                        "height": "sm",
-                        "adjustMode": "shrink-to-fit"
-                    }
-                    ]
+                    "style": "secondary",
+                    "height": "sm"
+                },
+                {
+                    "type": "button",
+                    "action": {
+                    "type": "message",
+                    "label": "右側",
+                    "text": "座位で右側から診断"
+                    },
+                    "style": "secondary",
+                    "height": "sm",
+                    "adjustMode": "shrink-to-fit"
                 }
                 ]
+            }
+            ]
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": []
+        },
+        "styles": {
+            "body": {
+            "backgroundColor": "#F4F3F9"
             },
             "footer": {
-                "type": "box",
-                "layout": "vertical",
-                "contents": []
-            },
-            "styles": {
-                "body": {
-                "backgroundColor": "#F4F3F9"
-                },
-                "footer": {
-                "separator": True,
-                "backgroundColor": "#F4F3F9"
-                }
+            "separator": True,
+            "backgroundColor": "#F4F3F9"
             }
+        }
         }
     return dict1
 
