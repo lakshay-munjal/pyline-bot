eb3936e62e7f333095822b2b008b734b8545efa7
hp <shreshth29sapra@gmail.com>
2022-06-28 23:59:27 +0530

    ff

--- util.py
+++ util.py
@@ -83,6 +83,129 @@ def func():
 
 
 
+def func2():
+    dict1 = {
+        "type": "bubble",
+        "size": "kilo",
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+            {
+                "type": "text",
+                "text": "食事",
+                "weight": "bold",
+                "size": "lg",
+                "align": "center"
+            },
+            {
+                "type": "text",
+                "text": "下からご希望の項目をお選びください。",
+                "wrap": True,
+                "align": "center",
+                "margin": "md"
+            },
+            {
+                "type": "separator",
+                "margin": "sm"
+            },
+            {
+                "type": "box",
+                "layout": "vertical",
+                "margin": "md",
+                "spacing": "sm",
+                "contents": [
+                {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "margin": "lg",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "・食事アンケート",
+                        "size": "md",
+                        "align": "start",
+                        "weight": "bold",
+                        "flex": 5,
+                        "gravity": "center",
+                        "margin": "sm",
+                        "wrap": True
+                    },
+                    {
+                        "type": "text",
+                        "text": "選択",
+                        "size": "lg",
+                        "color": "#111111",
+                        "align": "center",
+                        "flex": 2,
+                        "offsetStart": "12px",
+                        "weight": "bold",
+                        "decoration": "underline",
+                        "action": {
+                        "type": "postback",
+                        "label": "action",
+                        "data": "shokuji="
+                        }
+                    }
+                    ]
+                },
+                {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "margin": "lg",
+                    "contents": [
+                    {
+                        "type": "text",
+                        "text": "・カスタマイズ",
+                        "size": "md",
+                        "align": "start",
+                        "weight": "bold",
+                        "flex": 5,
+                        "gravity": "center",
+                        "margin": "sm",
+                        "wrap": True
+                    },
+                    {
+                        "type": "text",
+                        "text": "選択",
+                        "size": "lg",
+                        "color": "#111111",
+                        "align": "center",
+                        "flex": 2,
+                        "offsetStart": "12px",
+                        "weight": "bold",
+                        "decoration": "underline",
+                        "action": {
+                        "type": "uri",
+                        "label": "action",
+                        "uri": "http://yahoo.co.jp/"
+                        }
+                    }
+                    ]
+                }
+                ]
+            }
+            ]
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": []
+        },
+        "styles": {
+            "body": {
+            "backgroundColor": "#F4F3F9"
+            },
+            "footer": {
+            "separator": True,
+            "backgroundColor": "#F4F3F9"
+            }
+        }
+        }
+    return dict1
+
+
+
 def simpleTextMessage(msg):
     dictResp = {
         "type": "bubble",
@@ -114,3 +237,5 @@ def simpleTextMessage(msg):
     }
 
     return dictResp 
+
+
