1a2fdc6299efaa4712b6b680250a9663c5cfa05b
hp <shreshth29sapra@gmail.com>
2022-07-04 00:50:44 +0530

    ff

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ def statehandle(event):
             respItems = []
             for item in motionopt:
                 respItems.append(item["itemName"])
-            response = util.singleElementofList(respItems)
+            response = util.listTextMessage(respItems)
             print("respppp")
             print(response)
             flag = True
@@ -416,7 +416,7 @@ def statehandle(event):
             print("qqq")
             print(questionaire)
             makeoptions(questionaire['questionItems'][0]['choiceItems'])
-            response = "アンケートを始めましょう: \n Q1) " + questionaire['questionItems'][0]['questionText'] + options
+            # response = "アンケートを始めましょう: \n Q1) " + questionaire['questionItems'][0]['questionText'] + options
             state_dict[event.source.user_id]['state']= 'questionaire'
             # line_bot_api.set_default_rich_menu(back_menu_id)
 
